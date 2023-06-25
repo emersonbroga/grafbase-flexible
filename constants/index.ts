91668af2ae2113f80db38081bda3d76e24c05193
Emerson Broga <emerson@emersonbroga.com>
2023-06-24 22:25:42 -0300

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,182 @@
+import { FooterLink } from "@/common.types";
+
+export const NavLinks = [
+  { href: "/inspiration", key: "inspiration", text: "Inspiration" },
+  { href: "/find-projects", key: "find-projects", text: "Find Projects" },
+  {
+    href: "/learn-development",
+    key: "learn-development",
+    text: "Learn Development",
+  },
+  {
+    href: "/career-advancement",
+    key: "career-advancement",
+    text: "Career Advancement",
+  },
+  { href: "/hire-developers", key: "hire-developers", text: "Hire Developers" },
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots",
+];
+
+export const footerLinks: FooterLink[] = [
+  {
+    title: "For developers",
+    links: [
+      { href: "/", key: "Go Pro!", text: "Go Pro!" },
+      {
+        href: "/",
+        key: "Explore development work",
+        text: "Explore development work",
+      },
+      { href: "/", key: "Development blog", text: "Development blog" },
+      { href: "/", key: "Code podcast", text: "Code podcast" },
+      { href: "/", key: "Open-source projects", text: "Open-source projects" },
+      { href: "/", key: "Refer a Friend", text: "Refer a Friend" },
+      { href: "/", key: "Code of conduct", text: "Code of conduct" },
+    ],
+  },
+  {
+    title: "Hire developers",
+    links: [
+      {
+        href: "/post-a-job-opening",
+        key: "Post a job opening",
+        text: "Post a job opening",
+      },
+      {
+        href: "/post-a-freelance-project",
+        key: "Post a freelance project",
+        text: "Post a freelance project",
+      },
+      {
+        href: "/search-for-developers",
+        key: "Search for developers",
+        text: "Search for developers",
+      },
+    ],
+  },
+  {
+    title: "Brands",
+    links: [
+      {
+        href: "/advertise-with-us",
+        key: "Advertise with us",
+        text: "Advertise with us",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        href: "/about",
+        key: "About",
+        text: "About",
+      },
+      { href: "/careers", key: "Careers", text: "Careers" },
+      { href: "/support", key: "Support", text: "Support" },
+      { href: "/media-kit", key: "Media kit", text: "Media kit" },
+      { href: "/testimonials", key: "Testimonials", text: "Testimonials" },
+      { href: "/api", key: "API", text: "API" },
+      {
+        href: "/terms-of-service",
+        key: "Terms of service",
+        text: "Terms of service",
+      },
+      {
+        href: "/privacy-policy",
+        key: "Privacy policy",
+        text: "Privacy policy",
+      },
+      { href: "/cookie-policy", key: "Cookie policy", text: "Cookie policy" },
+    ],
+  },
+  {
+    title: "Directories",
+    links: [
+      {
+        href: "/development-jobs",
+        key: "Development jobs",
+        text: "Development jobs",
+      },
+      {
+        href: "/developers-for-hire",
+        key: "Developers for hire",
+        text: "Developers for hire",
+      },
+      {
+        href: "/freelance-developers-for-hire",
+        key: "Freelance developers for hire",
+        text: "Freelance developers for hire",
+      },
+      { href: "/tags", key: "Tags", text: "Tags" },
+      { href: "/places", key: "Places", text: "Places" },
+    ],
+  },
+  {
+    title: "Development assets",
+    links: [
+      {
+        href: "/code-marketplace",
+        key: "Code Marketplace",
+        text: "Code Marketplace",
+      },
+      {
+        href: "/github-marketplace",
+        key: "GitHub Marketplace",
+        text: "GitHub Marketplace",
+      },
+      { href: "/npm-registry", key: "NPM Registry", text: "NPM Registry" },
+      { href: "/packagephobia", key: "Packagephobia", text: "Packagephobia" },
+    ],
+  },
+  {
+    title: "Development Resources",
+    links: [
+      {
+        href: "/freelancing",
+        key: "Freelancing",
+        text: "Freelancing",
+      },
+      {
+        href: "/develoment-hiring",
+        key: "Development Hiring",
+        text: "Development Hiring",
+      },
+      {
+        href: "/development-portfolio",
+        key: "Development Portfolio",
+        text: "Development Portfolio",
+      },
+      {
+        href: "/development-education",
+        key: "Development Education",
+        text: "Development Education",
+      },
+      {
+        href: "/creative-process",
+        key: "Creative Process",
+        text: "Creative Process",
+      },
+      {
+        href: "/development-industry-trends",
+        key: "Development Industry Trends",
+        text: "Development Industry Trends",
+      },
+    ],
+  },
+];
